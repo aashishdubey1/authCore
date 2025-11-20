3707cc8de5ee02d0582016a028d3e53dc9a77094
Aashish <aashishdubey.dev@gmail.com>
2025-11-21 02:03:10 +0530

    added input validation for register

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,43 +1,46 @@
 import { Router } from "express";
+import { register } from "../controller/auth.controller";
+import { validate } from "../middlewares/inputValidate";
+import { userRegistrationSchema } from "../validation/auth.schema";
 
 const router = Router();
 
-router.post("/register", () => {});
+router.post("/register", validate(userRegistrationSchema), register);
 
-router.post("/login", () => {});
+// router.post("/login", () => {});
 
-router.post("/logout", () => {});
+// router.post("/logout", () => {});
 
-router.post("/logout-all", () => {});
+// router.post("/logout-all", () => {});
 
-router.post("/refresh", () => {});
+// router.post("/refresh", () => {});
 
-router.get("/verify-email/:token", () => {});
+// router.get("/verify-email/:token", () => {});
 
-router.post("/resend-verification", () => {});
+// router.post("/resend-verification", () => {});
 
-router.post("/forgot-password", () => {});
+// router.post("/forgot-password", () => {});
 
-router.post("/change-password", () => {});
+// router.post("/change-password", () => {});
 
-router.post("/reset-password", () => {});
+// router.post("/reset-password", () => {});
 
-router.get("/sessions", () => {});
+// router.get("/sessions", () => {});
 
-router.delete("/sessions/:sessionId");
+// router.delete("/sessions/:sessionId");
 
-router.get("/session/current");
+// router.get("/session/current");
 
-router.post("/mfa/setup", () => {});
+// router.post("/mfa/setup", () => {});
 
-router.post("/mfa/verify", () => {});
+// router.post("/mfa/verify", () => {});
 
-router.post("/mfa/verify-login", () => {});
+// router.post("/mfa/verify-login", () => {});
 
-router.post("/mfa/disable", () => {});
+// router.post("/mfa/disable", () => {});
 
-router.post("/mfa/regenerate-backup-codes", () => {});
+// router.post("/mfa/regenerate-backup-codes", () => {});
 
-router.post("/mfa/verify-backup-code", () => {});
+// router.post("/mfa/verify-backup-code", () => {});
 
 export default router;
---
+++ src/validation/auth.schema.ts
@@ -0,0 +1,26 @@
+import z from "zod";
+
+export const userRegistrationSchema = z.object({
+  name: z.string("Name is required").max(100, "Name is too long"),
+  email: z.email("Email is required"),
+  password: z
+    .string("Password is required")
+    .min(8, "Minimumm length should be 8 characters")
+    .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+    .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+    .regex(/[0-9]/, "Password must contain at least one Number")
+    .regex(
+      /[^A-Za-z0-9]/,
+      "Password must contain at least one special character"
+    ),
+});
+
+export const userLoginSchema = z.object({
+  email: z.email("Email is required"),
+  password: z
+    .string("Password is required")
+    .min(8, "Minimumm length should be 8 characters"),
+});
+
+export type UserRegisterInput = z.infer<typeof userRegistrationSchema>;
+export type UserLoginInput = z.infer<typeof userLoginSchema>;
