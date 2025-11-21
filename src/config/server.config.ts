73ccc6c74ad9fee29078a478049d5a4690ad8308
Aashish <aashishdubey.dev@gmail.com>
2025-11-21 12:45:22 +0530

    enabled email service for email verification

--- src/config/server.config.ts
+++ src/config/server.config.ts
@@ -1,4 +1,8 @@
 export default {
   DB_URL: process.env.DB_URL,
   PORT: process.env.PORT,
+  SALT_ROUND: process.env.SALT_ROUND,
+  RESEND_API_KEY: process.env.RESEND_API_KEY,
+  EMAIL_FROM: process.env.EMAIL_FROM,
+  APP_URL: process.env.APP_URL,
 };
---
+++ src/utils/emailService.ts
@@ -0,0 +1,28 @@
+import { Resend } from "resend";
+import serverConfig from "../config/server.config";
+
+const resend = new Resend(serverConfig.RESEND_API_KEY);
+
+class EmailService {
+  async sendVerificaitonEmail(email: string, token: string) {
+    const verifyUrl = `${serverConfig.APP_URL}/verify-email?token=${token}`;
+    const html = `
+      <h2>Verify your email</h2>
+      <p>Click the link below to verify your email:</p>
+      <a href="${verifyUrl}">${verifyUrl}</a>
+    `;
+
+    try {
+      await resend.emails.send({
+        from: serverConfig.EMAIL_FROM!,
+        to: email,
+        subject: "Verify your email address ",
+        html,
+      });
+    } catch (error) {
+      console.error("Email sending failed:", error);
+    }
+  }
+}
+
+export const emailService = new EmailService();
